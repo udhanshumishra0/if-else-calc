9ee696926f913a979880356e68a3525495613f58
udhanshumishra0 <udhanshu.mishra0@gmail.com>
2025-03-18 15:50:11 +0000

    nothing

---
+++ last pr/function.py
@@ -0,0 +1,11 @@
+def iseven(num):
+
+    if num % 2 == 0:  
+
+        print("It's an even number")  # Properly indented
+        
+    else:  
+        print("Not even")  # Properly indented
+
+iseven(6)  # Output: It's an even number
+iseven(7)  # Output: Not even
